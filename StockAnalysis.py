4280f006d197d984658219c328f6808e857d0ce9
akalia25 <32119652+akalia25@users.noreply.github.com>
2019-11-11 23:57:13 -0500

    user_input
    getting user's input for the stocks

--- StockAnalysis.py
+++ StockAnalysis.py
@@ -6,3 +6,19 @@ Created on Mon Nov 11 23:37:01 2019
 @author: adityakalia
 """
 
+import yfinance as yf
+
+def user_Stocks():
+    while True :
+        try : 
+            stocks =  input("Please enter the stocks you would like to analyze seperated by commas : ")
+            if len(stocks) > 0 :
+                break
+        except :
+            pass
+        print("Incorrect input please enter your stocks")
+    
+    return stocks
+ 
+
+    

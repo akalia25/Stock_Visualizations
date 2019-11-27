f0c599d23de1608a45e4552ac95d2b8bf85127f8
akalia25 <32119652+akalia25@users.noreply.github.com>
2019-11-26 20:20:34 -0500

    Correlation and Covariance
    Updates on correlation and covariance function + updated error catching when taking in stock input

--- StockAnalysis.py
+++ StockAnalysis.py
@@ -11,6 +11,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as matdates
 
+
 def user_input():
     """
     This function takes the user's input of what stocks they would like to
@@ -44,11 +45,15 @@ def historicalData(stocks):
     """
     df = pd.DataFrame()
     for x in stocks:
-        stock = yf.Ticker(x)
-        tempdf = stock.history(period='3mo')
-        tempdf.loc[:, 'StockName'] = x
-        df = df.append(tempdf, sort='False')
-        df.loc[:, 'ROI'] = df['Close'].pct_change()
+        try:
+            stock = yf.Ticker(x)
+            tempdf = stock.history(period='3mo')
+            tempdf.loc[:, 'StockName'] = x
+            df = df.append(tempdf, sort='False')
+            df.loc[:, 'ROI'] = df['Close'].pct_change()
+        except ValueError:
+            print("Incorrect stock entered " + x)
+            pass
     return df
 
 
@@ -64,11 +69,15 @@ def stockForecastingMovingAverage(stocks_df):
         df1.reset_index(inplace=True)
         data = df1[['Date', 'Close']]
         for i in (5, 10, 15):
-            simple_moving_average = data.set_index('Date').rolling(window=i).mean()
-            df1.loc[:, 'SMA ' + str(stock) + ' ' + str(i)] = simple_moving_average.values
+            simple_moving_average = data.set_index(
+                    'Date').rolling(window=i).mean()
+            df1.loc[:, 'SMA ' + str(stock) + ' ' + str(
+                    i)] = simple_moving_average.values
         for i in (5, 10, 15):
-            exponential_moving_average = data.set_index('Date').ewm(span=i, adjust=False).mean()
-            df1.loc[:, 'EMA ' + str(stock) + ' ' + str(i)] = exponential_moving_average.values
+            exponential_moving_average = data.set_index(
+                    'Date').ewm(span=i, adjust=False).mean()
+            df1.loc[:, 'EMA ' + str(stock) + ' ' + str(
+                    i)] = exponential_moving_average.values
         print(df1)
 
 
@@ -86,16 +95,17 @@ def stockBollingerBands(stocks_df):
         df.loc[:, '20std'] = df['Close'].rolling(window=20).std()
         df.loc[:, 'Upper Bound'] = df['Middle Bound'] + (df['20std'] * 2)
         df.loc[:, 'Lower Bound'] = df['Middle Bound'] - (df['20std'] * 2)
-        fig = plt.figure() #
+        fig = plt.figure()
         ax = fig.add_subplot(111)
-        #https://stackoverflow.com/a/37219987 reference of the code
+        # https://stackoverflow.com/a/37219987 reference of the code
         majorFmt = matdates.DateFormatter('%Y-%m-%d %H:%M')
         Daylocator2 = matdates.DayLocator(interval=1)
         ax.xaxis.set_minor_locator(Daylocator2)
         ax.xaxis.set_major_formatter(majorFmt)
         plt.setp(ax.xaxis.get_majorticklabels(), rotation=45)
         x_axis = df.index.get_level_values('Date')
-        ax.fill_between(x_axis, df['Upper Bound'], df['Lower Bound'], color='orange')
+        ax.fill_between(x_axis, df['Upper Bound'], df['Lower Bound'],
+                        color='orange')
         ax.plot(x_axis, df['Close'], color='blue', lw=2)
         ax.plot(x_axis, df['Upper Bound'], color='Black', lw=1)
         ax.plot(x_axis, df['Lower Bound'], color='Black', lw=1)
@@ -108,12 +118,32 @@ def stockBollingerBands(stocks_df):
         plt.show()
 
 
+def CovarianceCorrelation(stocks_df):
+    """
+    Covariance measures how related the stocks are to each other, value
+    of 0 meaning no relation, and a positive number indicating a positive
+    relation in the same direction.
+    Correlation measures a standarized value of the direction and strength
+    between the stocks, it is measured between -1 to +1.
+    """
+    returns = stocks_df[['ROI', 'StockName']]
+    returns = returns.pivot(columns='StockName', values='ROI')
+    cov = returns.cov()
+    print("The Covariance matrix is ")
+    print(cov.rename_axis(None))
+    corr = returns.corr()
+    print("\n The Correlation matrix is ")
+    print(corr.rename_axis(None))
+    return cov, corr
+
+
 def main():
     stocks = user_input().split(',')
     stocks = parseStocks(stocks)
     stocks_df = historicalData(stocks)
     stockForecastingMovingAverage(stocks_df)
     stockBollingerBands(stocks_df)
+    CovarianceCorrelation(stocks_df)
 
 if __name__ == '__main__':
     main()
